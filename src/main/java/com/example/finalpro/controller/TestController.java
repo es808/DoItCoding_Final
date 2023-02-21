8f4ccd217bc8d44e4e759f2b76f7f0da29461a87
yeongmin <dudals1069@gmail.com>
2023-02-21 16:03:44 +0900

    security_login

--- src/main/java/com/example/finalpro/controller/TestController.java
+++ src/main/java/com/example/finalpro/controller/TestController.java
@@ -5,10 +5,12 @@ import com.example.finalpro.entity.Customer;
 import com.example.finalpro.service.CustomerService;
 import com.example.finalpro.service.CategoryService;
 import com.example.finalpro.service.TicketService;
-import com.example.finalpro.vo.CustomerVO;
 import jakarta.servlet.http.HttpSession;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -83,8 +85,8 @@ public class TestController {
         ModelAndView mav = new ModelAndView("redirect:/login");
         m.setPwd(passwordEncoder.encode(m.getPwd()));
         m.setCateid(1);
-        m.setEmail("Test");
-        m.setPhone("010123");
+        m.setEmail("Test123");
+        m.setPhone("0101234");
         m.setGender("a");
         Date d = new Date();
 
@@ -107,26 +109,46 @@ public class TestController {
         return mav ;
     }
 
-    @PostMapping("/login_submit")
-    public ModelAndView loginSubmit(String id, String pwd, HttpSession session){
-        System.out.println("login실행");
-        System.out.println(id + "\n" + pwd);
-        ModelAndView mav = new ModelAndView("redirect:/");
-
-        Optional<Customer> option = customerDAO.findById(id);
-        if(option.isPresent()){
-            String dbPwd = option.get().getPwd();
-            System.out.println(dbPwd);
-            if(pwd.equals(dbPwd)){
-                session.setAttribute("id", id);
-            }else{
-                mav.setViewName("redirect:/login");
-            }
-        }else{
-            mav.addObject("msg", "로그인실패.");
-            mav.setViewName("error");
-        }
-        return mav;
+    @GetMapping("/service1")
+    public void service1(HttpSession session) {
+//        //인증된(로그인한) 회원의 정보를 가져오기 위하여
+//        //시큐리티의 인증객체가 필요.
+//        Authentication authentication =
+//                SecurityContextHolder.getContext().getAuthentication();
+//
+//        //이 인증객체를 통해서 인증된(로그인된) User객체를 받아 온다.
+//        User user = (User)authentication.getPrincipal();
+//
+//        //이 인증된 User를 통해서 로그인된 회원의 아이디를 가져온다.
+//
+//        String id = user.getUsername();
+//        //아이디 정보를 세션에 상태유지 한다.
+//        //만약, id뿐 아니라 로그인한 회원의 다른정보도 필요하다면 dao를 통해 회원 정보를 가져와서 상태유지
+//
+//        session.setAttribute("id", id);
     }
+
+
+//    @PostMapping("/login")
+//    public ModelAndView loginSubmit(String id, String pwd, HttpSession session){
+//        System.out.println("login실행");
+//        System.out.println(id + "\n" + pwd);
+//        ModelAndView mav = new ModelAndView("redirect:/");
+//
+//        Optional<Customer> option = customerDAO.findById(id);
+//        if(option.isPresent()){
+//            String dbPwd = option.get().getPwd();
+//            System.out.println(dbPwd);
+//            if(pwd.equals(dbPwd)){
+//                session.setAttribute("id", id);
+//            }else{
+//                mav.setViewName("redirect:/login");
+//            }
+//        }else{
+//            mav.addObject("msg", "로그인실패.");
+//            mav.setViewName("error");
+//        }
+//        return mav;
+//    }
 
 }
--- src/main/java/com/example/finalpro/entity/Customer.java
+++ src/main/java/com/example/finalpro/entity/Customer.java
@@ -3,12 +3,16 @@ package com.example.finalpro.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "customer")
 public class Customer {
     //JPA는 테이블 속성명과 엔티티의 변수명을 똑같이해야 인식함.
@@ -20,5 +24,7 @@ public class Customer {
     private String email;
     private String phone;
     private String gender;
+    private String role;
+    private String favor;
     private int cateid;
 }
