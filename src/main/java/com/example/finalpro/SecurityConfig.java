42146aae13013f338c465e64402741317eb00b94
yeongmin <dudals1069@gmail.com>
2023-02-21 17:09:12 +0900

    security_login

--- src/main/java/com/example/finalpro/SecurityConfig.java
+++ src/main/java/com/example/finalpro/SecurityConfig.java
@@ -20,13 +20,13 @@ public class SecurityConfig {
                 .anyRequest().authenticated();
 
         http.formLogin().loginPage("/login").permitAll()
-                .defaultSuccessUrl("/service1");
+                .defaultSuccessUrl("/service1",true);        //로그인 성공 후 이동 페이지 true를 붙여서 붙여서 절대경로 설정
 
 
-//        http.logout()
-//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//                .invalidateHttpSession(true)
-//                .logoutSuccessUrl("/");
+        http.logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .invalidateHttpSession(true)
+                .logoutSuccessUrl("/");
 
         http.httpBasic();
 
