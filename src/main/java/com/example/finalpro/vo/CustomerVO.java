07bbdb521610d5576474ad07696f389363f6d9f0
yeongmin <dudals1069@gmail.com>
2023-02-23 14:20:01 +0900

    2/23 security login, signup, entity, vo

--- src/main/java/com/example/finalpro/vo/CustomerVO.java
+++ src/main/java/com/example/finalpro/vo/CustomerVO.java
@@ -7,12 +7,13 @@ import java.util.Date;
 @Data
 public class CustomerVO {
     private String custid;
+    private int cateid;
     private String pwd;
     private String name;
     private String birth;
-    private String email;
     private String phone;
+    private String email;
+    private String addr;
     private String gender;
-    private int cateid;
-    private String favor;
+    private String role;
 }
