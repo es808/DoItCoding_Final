7ec4d0f16d26d79a5b02f9dfca28d55b105f6396
yeongmin <dudals1069@gmail.com>
2023-03-03 02:01:23 +0900

    draw end

--- src/main/java/com/example/finalpro/controller/DrawController.java
+++ src/main/java/com/example/finalpro/controller/DrawController.java
@@ -44,54 +44,48 @@ public class DrawController {
         return DBManager.findByTicketid(ticketid);
     }
 
-    @GetMapping("/drawTest")
-    public String drawTest(Model m){
-        List<Draw> draw = null;
-        draw = DBManager.drawTest(1);
-        m.addAttribute("list",draw);
-        return "drawTest/drawTest";
-    }
-
+    //드로우 신청한 회원정보 가져오기
     @GetMapping("/drawList")
     @ResponseBody
-    public List<DrawVO> drawList(){
+    public List<DrawVO> drawList(int ticketid){
         List<DrawVO> draw = null;
-        draw = DBManager.drawTest(1);
+        draw = DBManager.drawTest(ticketid);
         return draw;
     }
-    
-    //drawTest2
+
+    //드로우 가동
     @GetMapping("/drawTest2")
     @ResponseBody
-    public void drawTest2(){
+    public String[] drawTest2(){
         int count = DBManager.findLeftSeatByTicketid(1);
-        List<DrawVO> draw = drawList();
+        List<DrawVO> draw = drawList(1);
         System.out.println("드로우 사이즈:"+draw.size());
-        String arr[] = new String[count];
+        String arr[] = new String[count];           //드로우에 당첨된 회원아이디 저장
 
         LinkedList<DrawVO> list = new LinkedList<>();
 
         for(DrawVO d : draw){
             list.add(d);
         }
-
         System.out.println(list);
-
-
-
-
 
         for (int i = 0; i < count; i++){
             Random r = new Random();
-            int number = r.nextInt(list.size());
-            arr[i] = list.get(number).getCustid();
-            list.remove(number);
+            int number = r.nextInt(list.size());        //랜덤함수를 드로우를 신청한 회원 수 만큼의 크기로 설정
+            arr[i] = list.get(number).getCustid();      //랜덤함수에 배정된 회원아이디를 배열에 저장
+            list.remove(number);                        //당첨된 회원은 linkedList에서 제거하고 다시 반복문 가동
             System.out.println(list.size());
         }
-
-        for(int i = 0; i < count; i++){
-            System.out.println(arr[i]);
-        }
-        System.out.println(count);
+        return arr;
     }
+
+    @GetMapping("/drawTest")
+    public String drawTest(Model m){
+        String arr[] = drawTest2();
+        m.addAttribute("list", arr);
+        System.out.println(arr[1]);
+        return "drawTest/drawTest";
+    }
+
+
 }
